e41eaaade8d992163f582ba825a3ac72cc0e4d62
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-08 04:02:17 +0530

    finding missing number in an array basic method

--- main.cpp
+++ main.cpp
@@ -5,38 +5,18 @@
 using namespace std;
 int main()
 {
-   /***************** finding the first repeated number in array */////////////
-   int *array_,n;
+   /***************** finding missing number in an array*/////////////
+   int n,*arr,i;
    cin>>n;
-   map<int,int> map_;
-   for(int i=0;i<n;i++)
-    cin>>array_[i];
-   for(int i=0;i<n;i++)
-    {
-        if(map_.find(array_[i])!=map_.end())
-        {
-            if(map_[array_[i]]<0)
-                continue;
-            else
-                map_[array_[i]]=-map_[array_[i]];
-        }
-        else
-            map_[array_[i]]=i+1;
+   arr=new int[n];
+   for(i=1;i<n;i++)
+    cin>>arr[i];
+   int sum=0;
+   for(int i=1;i<=n;i++)
+    sum+=i;
+   for(i=1;i<n;i++)
+    sum-=arr[i];
+   cout<<sum;
 
-    }
-    map<int,int>::iterator it=map_.begin();
-    int a,b=INT_MIN;
-    for(it;it!=map_.end();it++)
-    {
-        cout<<"a";
-        if(it->second<0)
-        {
- if(it->second>b)
-    {
-        a=it->first;
-        b=it->second;
-    }
-        }
-    }
-   cout<<a<< " "<<b;
+
 }
