189d6f935add44812cf8d12180969cbac5e52e7d
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-08 10:58:44 +0530

    given an array find two numbers in the array such that their sum is equal to k using hashing

--- main.cpp
+++ main.cpp
@@ -5,29 +5,29 @@
 using namespace std;
 int main()
 {
-   /***************** given an array find two numbers in the array such that their sum is equal to k*/////////////
+   /***************** given an array find two numbers in the array such that their sum is equal to k using hashing*/////////////
    int i,n,*arr,j,k;
    cin>>n>>k;
    arr=new int[n];
+   map<int,int> map_;
    for(i=0;i<n;i++)
-    cin>>arr[i];
-   sort(arr,arr+n);
-   for(i=0,j=n-1;i<j;)
    {
-       int sum=arr[i]+arr[j];
-       if(sum==k)
+        cin>>arr[i];
+        map_.insert(pp(arr[i],i));
+   }
+   /*map<int,int>::iterator it=map_.begin();
+   for(it;it!=map_.end();it++)
+   {
+       if(map)
+   }*/
+   for(i=0;i<n;i++)
+   {
+       if((map_.find(arr[i])!=map_.end())&&(map_.find(k-arr[i])!=map_.end()))
        {
-           cout<<"yes result found"<<i<<" "<<j;
+           cout<<"yes "<<map_[arr[i]]<<" "<<map_[k-arr[i]];
            break;
        }
-       else
-        if(sum>k)
-       {
-           j--;
-       }
-       else
-        i++;
-
    }
+
 
 }
