8bd15136d2eb963762d5010c7da3fac385b08b7c
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-08 09:45:39 +0530

    finding number occuring odd number of times

--- main.cpp
+++ main.cpp
@@ -5,16 +5,14 @@
 using namespace std;
 int main()
 {
-   /***************** finding missing number in an array using hashing type technique*/////////////
+   /***************** finding number occuring odd number of times*/////////////
    int n,*arr,i,res=0;
    cin>>n;
    arr=new int[n];
-   for(i=1;i<n;i++)
-    cin>>arr[i];
-   for(i=1;i<n;i++)
-    res=res^arr[i];
-   for(i=1;i<=n;i++)
-    res=res^i;
-   cout<<res;
-
+   for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        res=res^arr[i];
+    }
+    cout<<res;
 }
