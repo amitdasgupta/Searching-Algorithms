a94dfd15021d51bc151d66f2ec2a33646e5ef1b4
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-08 10:11:17 +0530

    finding two repeating elements in the array using xor technique

--- main.cpp
+++ main.cpp
@@ -5,14 +5,27 @@
 using namespace std;
 int main()
 {
-   /***************** finding number occuring odd number of times*/////////////
-   int n,*arr,i,res=0;
-   cin>>n;
-   arr=new int[n];
-   for(i=0;i<n;i++)
+   /***************** finding two repeating elements in the array*/////////////
+   int n,size_,*arr,i,res=0;
+   cin>>size_>>n;
+   arr=new int[size_];
+   for(i=1;i<=size_;i++)
     {
         cin>>arr[i];
         res=res^arr[i];
     }
-    cout<<res;
+   for(int i=1;i<=n;i++)
+   {
+       res=res^i;
+   }
+   int right_most_set_bit=(res)&~(res-1);
+   int x=0,y=0;
+   for(i=1;i<=size_;i++)
+    {
+        if(arr[i]&right_most_set_bit)
+            x^=arr[i];
+        else
+            y^=arr[i];
+    }
+    cout<<x<<" "<<y;
 }
