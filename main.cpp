ecfe4ae00ca76d9e6135718f99db641bca7610c4
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-08 19:52:28 +0530

    given an array find two numbers in the array such that their sum is closest to zero

--- main.cpp
+++ main.cpp
@@ -6,28 +6,47 @@ using namespace std;
 int main()
 {
    /***************** given an array find two numbers in the array such that their sum is equal to k using hashing*/////////////
-   int i,n,*arr,j,k;
-   cin>>n>>k;
-   arr=new int[n];
-   map<int,int> map_;
-   for(i=0;i<n;i++)
+   int i,ni,*arr,j,k;
+   cin>>ni;
+   arr=new int[ni];
+   for(i=0;i<ni;i++)
    {
         cin>>arr[i];
-        map_.insert(pp(arr[i],i));
    }
-   /*map<int,int>::iterator it=map_.begin();
-   for(it;it!=map_.end();it++)
+   sort(arr,arr+ni);
+   int p=INT_MAX,n=INT_MIN,temp;
+   j=ni-1;
+   i=0;
+   for(;i<j;)
    {
-       if(map)
-   }*/
-   for(i=0;i<n;i++)
-   {
-       if((map_.find(arr[i])!=map_.end())&&(map_.find(k-arr[i])!=map_.end()))
+
+
+       temp=arr[i]+arr[j];
+       cout<<p<<" "<<n<<" "<<temp<<endl;
+       if(temp>0)
        {
-           cout<<"yes "<<map_[arr[i]]<<" "<<map_[k-arr[i]];
+           if(temp<p)
+            p=temp;
+           j--;
+       }
+       else
+        if(temp<0)
+       {
+           if(temp>n)
+           {
+             n=temp;
+           }
+           i++;
+       }
+       else
+       {
+           p=0;
+           n=0;
            break;
        }
    }
+   (abs(p)<abs(n))?cout<<abs(p):cout<<abs(n);
+
 
 
 }
