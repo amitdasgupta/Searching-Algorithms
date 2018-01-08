8d728eb977adbb2a1d7c70cce614c4d853566027
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-08 09:40:41 +0530

    finding missing number in an array using xor  gate technique

--- main.cpp
+++ main.cpp
@@ -5,20 +5,16 @@
 using namespace std;
 int main()
 {
-   /***************** finding missing number in an array*/////////////
-   int n,*arr,i,*arr2;
+   /***************** finding missing number in an array using hashing type technique*/////////////
+   int n,*arr,i,res=0;
    cin>>n;
    arr=new int[n];
-   arr2=new int[n+1]();
    for(i=1;i<n;i++)
     cin>>arr[i];
    for(i=1;i<n;i++)
-    arr2[arr[i]]=1;
+    res=res^arr[i];
    for(i=1;i<=n;i++)
-    if(!arr2[i])
-   {
-       cout<<i;
-       break;
-   }
+    res=res^i;
+   cout<<res;
 
 }
