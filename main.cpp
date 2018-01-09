4f3e6cd56f5298bc34279e92f92191934645a4e2
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-10 05:08:27 +0530

    finding bitonic point in bitonic array if array is of infinite size or unknown size
    special case consisting of 0 and 1 only

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include<algorithm>
 #define pp pair<int,int>
 using namespace std;
-/***************** finding key index in bitonic array*/////////////
+/***************** finding bitonic point in bitonic array if array is of infinite size or unknown size*/////////////
+
 int indexOfBitonicArray(int *arr,int start,int last)
 {
     if(start==last)
@@ -20,17 +21,27 @@ int indexOfBitonicArray(int *arr,int start,int last)
     else
     {
         int mid=(last+start)/2;
-       if(arr[mid-1]<arr[mid]&&arr[mid]>arr[mid+1])
+       if(arr[mid-1]<=arr[mid]&&arr[mid]>arr[mid+1])
         return mid;
        else
-        if(arr[mid-1]<arr[mid]&&arr[mid]<arr[mid+1])
+        if(arr[mid-1]<=arr[mid]&&arr[mid]<=arr[mid+1]&&arr[mid]==1)
         return indexOfBitonicArray(arr,mid+1,last);
-       else
-        if(arr[mid-1]>arr[mid]&&arr[mid]>arr[mid+1])
-        return indexOfBitonicArray(arr,start,mid-1);
+  /*     else
+        if(arr[mid-1]>=arr[mid]&&arr[mid]>=arr[mid+1])
+        return indexOfBitonicArray(arr,start,mid-1);*/
         else
         return INT_MIN;
     }
+}
+int infiniteBitonicHelp(int *arr,int start,int last)
+{
+    while(arr[last]>=arr[start])
+    {
+        start=last;
+        last=2*last;
+    }
+    cout<<start<<" "<<last;
+    return indexOfBitonicArray(arr,start,last);
 }
 int main()
 {
@@ -39,5 +50,6 @@ int main()
    arr=new int[n];
    for(i=0;i<n;i++)
     cin>>arr[i];
-   cout<<indexOfBitonicArray(arr,0,n-1);
+   cout<<"bitonic index is:"<<infiniteBitonicHelp(arr,0,1);
+   delete[] arr;
 }
