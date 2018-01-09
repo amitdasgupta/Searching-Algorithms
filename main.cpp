ca1cf2fee6353cf59dc6b2215de13551065fdda5
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-10 04:25:08 +0530

    finding bitonic point in bitonic array

--- main.cpp
+++ main.cpp
@@ -4,57 +4,40 @@
 #include<algorithm>
 #define pp pair<int,int>
 using namespace std;
-/***************used it to avoid overflow*/////////////////////
-unsigned int absu(int value) {
-    return (value < 0) ? -((unsigned int)value) : (unsigned int)value;
+/***************** finding key index in bitonic array*/////////////
+int indexOfBitonicArray(int *arr,int start,int last)
+{
+    if(start==last)
+        return start;
+    else
+        if(last-start==1)
+    {
+        if(arr[start]>arr[last])
+            return start;
+        else
+        return last;
+    }
+    else
+    {
+        int mid=(last+start)/2;
+       if(arr[mid-1]<arr[mid]&&arr[mid]>arr[mid+1])
+        return mid;
+       else
+        if(arr[mid-1]<arr[mid]&&arr[mid]<arr[mid+1])
+        return indexOfBitonicArray(arr,mid+1,last);
+       else
+        if(arr[mid-1]>arr[mid]&&arr[mid]>arr[mid+1])
+        return indexOfBitonicArray(arr,start,mid-1);
+        else
+        return INT_MIN;
+    }
 }
 int main()
 {
-   /***************** find three numbers in an array whose sum is closest to zero*/////////////
-   int i,ni,*arr,j,k,q,flag=0,l,h,temp=0,positive=INT_MAX,negative=INT_MIN;
-   cin>>ni;
-   arr=new int[ni];
-   for(i=0;i<ni;i++)
-   {
-        cin>>arr[i];
-   }
-   sort(arr,arr+ni);
-   cout<<"sorted array is:"<<endl;
-   for(i=0;i<ni;i++)
-   {
-        cout<<arr[i]<<" ";
-   }
-   cout<<"\n";
-   for(i=0;i<ni-2&&!flag;i++)
-   {
-       l=i+1;
-       h=ni-1;
-       for(;l<h&&!flag;)
-       {
-           //cout<<positive<<" "<<negative<<endl;
-            temp=arr[i]+arr[l]+arr[h];
-           if(temp>0)
-           {
-               if(temp<positive)
-                positive=temp;
-               h--;
-           }
-           else
-           if(temp<0)
-           {
-               if(temp>negative)
-                negative=temp;
-               l++;
-           }
-           else
-           {
-               positive=0;
-               negative=0;
-               flag=1;
-           }
-    }
-
-   }
-
-   (absu(positive<absu(negative)))?cout<<positive:cout<<negative;
+   int n,*arr,i;
+   cin>>n;
+   arr=new int[n];
+   for(i=0;i<n;i++)
+    cin>>arr[i];
+   cout<<indexOfBitonicArray(arr,0,n-1);
 }
