172aea6435532629071daacfc9ddfc677b2f4460
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-09 17:59:50 +0530

    find three numbers in an array whose sum is equal to k

--- main.cpp
+++ main.cpp
@@ -5,48 +5,41 @@
 using namespace std;
 int main()
 {
-   /***************** given an array find two numbers in the array such that their sum is equal to k using hashing*/////////////
-   int i,ni,*arr,j,k;
-   cin>>ni;
+   /***************** find three numbers in an array whose sum is equal to k*/////////////
+   int i,ni,*arr,j,k,q,flag=0,l,h,temp=0;
+   cin>>ni>>k;
    arr=new int[ni];
    for(i=0;i<ni;i++)
    {
         cin>>arr[i];
    }
    sort(arr,arr+ni);
-   int p=INT_MAX,n=INT_MIN,temp;
-   j=ni-1;
-   i=0;
-   for(;i<j;)
+   cout<<"sorted array is:"<<endl;
+   for(i=0;i<ni;i++)
    {
-
-
-       temp=arr[i]+arr[j];
-       cout<<p<<" "<<n<<" "<<temp<<endl;
-       if(temp>0)
-       {
-           if(temp<p)
-            p=temp;
-           j--;
-       }
-       else
-        if(temp<0)
-       {
-           if(temp>n)
-           {
-             n=temp;
-           }
-           i++;
-       }
-       else
-       {
-           p=0;
-           n=0;
-           break;
-       }
+        cout<<arr[i];
    }
-   (abs(p)<abs(n))?cout<<abs(p):cout<<abs(n);
+   cout<<"\n";
+   for(i=0;i<ni-2&&!flag;i++)
+   {
+       l=i+1;
+       h=ni-1;
+       for(;l<h&&!flag;)
+       {   temp=arr[i]+arr[l]+arr[h];
+           if(temp==k)
+           {
+               flag=1;
+           }
+           else
+           if(temp>k)
+           {
+               h--;
+           }
+           else
+            l++;
+    }
 
-
-
+   }
+   if(flag)
+   cout<<"yes"<<i-1<< " "<<l<<" "<<h;
 }
