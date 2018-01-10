51ee8d4ca41ea852bd6355260b5ddbd74125452b
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-10 14:52:58 +0530

    given an array which is rotated many times find the index of any given element

--- main.cpp
+++ main.cpp
@@ -4,7 +4,56 @@
 #include<algorithm>
 #define pp pair<int,int>
 using namespace std;
-/***************** finding bitonic point in bitonic array if array is of infinite size or unknown size*/////////////
+/**************given an array which is rotated many times find the index of any given element*///////////////
+int indexOfArray(int *arr,int start,int last)
+{
+    if(start==last)
+        return INT_MIN;
+    else
+        if(last-start==1)
+    {
+        if(arr[start]>arr[last])
+            return start;
+        else return INT_MIN;
+    }
+    else
+    {
+        int mid=(start+last)/2;
+        if(arr[mid+1]<arr[mid])
+            return mid;
+        else
+            if(arr[mid-1]>arr[mid])
+            return mid-1;
+        else
+        if(arr[start]<arr[mid]&&arr[mid]>arr[last])
+        return indexOfArray(arr,mid+1,last);
+        else
+        if(arr[start]>arr[mid]&&arr[mid]<arr[last])
+        return indexOfArray(arr,start,mid-1);
+        else
+            return INT_MIN;
+    }
+}
+int binarySearch(int *arr,int start,int last,int data)
+{
+
+    if(start>last)
+    {
+     return INT_MIN;
+    }
+    else
+    {
+        int mid=(start+last)/2;
+        if(arr[mid]==data)
+        return mid;
+        else
+            if(arr[mid]<data)
+            return binarySearch(arr,mid+1,last,data);
+            else
+            return binarySearch(arr,start,mid-1,data);
+    }
+}
+/***************** finding bitonic point in bitonic array if array is of infinite size or unknown size
 
 int indexOfBitonicArray(int *arr,int start,int last)
 {
@@ -26,9 +75,9 @@ int indexOfBitonicArray(int *arr,int start,int last)
        else
         if(arr[mid-1]<=arr[mid]&&arr[mid]<=arr[mid+1]&&arr[mid]==1)
         return indexOfBitonicArray(arr,mid+1,last);
-  /*     else
-        if(arr[mid-1]>=arr[mid]&&arr[mid]>=arr[mid+1])
-        return indexOfBitonicArray(arr,start,mid-1);*/
+  //     else
+    //    if(arr[mid-1]>=arr[mid]&&arr[mid]>=arr[mid+1])
+      //  return indexOfBitonicArray(arr,start,mid-1);
         else
         return INT_MIN;
     }
@@ -42,14 +91,23 @@ int infiniteBitonicHelp(int *arr,int start,int last)
     }
     cout<<start<<" "<<last;
     return indexOfBitonicArray(arr,start,last);
-}
+}*/////////
 int main()
 {
-   int n,*arr,i;
+
+   int n,*arr,i,data;
    cin>>n;
    arr=new int[n];
    for(i=0;i<n;i++)
     cin>>arr[i];
-   cout<<"bitonic index is:"<<infiniteBitonicHelp(arr,0,1);
+   //cout<<"bitonic index is:"<<infiniteBitonicHelp(arr,0,1);
+   int pivot=indexOfArray(arr,0,n-1);
+   cout<<"\n enter data to be searched";
+   cin>>data;
+   cout<<endl;
+      if(binarySearch(arr,0,pivot,data)!=INT_MIN||binarySearch(arr,pivot+1,n-1,data)!=INT_MIN)
+      {
+          (binarySearch(arr,0,pivot,data)!=INT_MIN)?cout<<binarySearch(arr,0,pivot,data):cout<<binarySearch(arr,pivot+1,n-1,data);
+      }
    delete[] arr;
 }
